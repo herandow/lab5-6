bb89b37ac723dbfc08b217f2d5c6897567ea65bb
herandow <nikita080602@yandex.ru>
2025-06-02 21:50:08 +0500

    ALL

--- main.c
+++ main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+#include <string.h>
+
+void greet_user() {
+    char name[100];
+    printf("Enter your name: ");
+    scanf("%s", name);
+    printf("Hello, %s! Welcome to Git Lab.\n", name);
+}
 
 int main() {
-    printf("Hello, world!\n");
+    greet_user();
     return 0;
 }
